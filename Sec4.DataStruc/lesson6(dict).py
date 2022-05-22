c76364abfecc7debe99584e476ea88aa8b3318ee
MACBOOK_MAKO <>
2022-05-22 21:15:13 +0900

    [new]辞書型・集合型について

---
+++ Sec4.DataStruc/lesson6(dict).py
@@ -0,0 +1,54 @@
+# 辞書型
+from cgi import print_directory
+
+
+d = {'x': 10, 'y': 20 }
+print(type(d))
+print(d['x'])
+print(d['y'])
+d['x'] = 100
+print(d)
+d['x'] = 'XXXX'
+d['z'] = 200
+print(d)
+
+d2 = dict( a=10, b=20)
+print(d2)
+
+
+# 辞書型のメソッド
+d = {'x': 10, 'y': 20 }
+
+print(d.keys())
+print(d.values())
+d2 = {'x': 1000, 'z': 500 }
+d.update(d2)
+print(d)
+print(d.get('x'))
+print(d.get('j'))
+
+d.pop('x')
+print(d)
+del d['y']
+print(d)
+d.clear()
+print(d)
+
+d = {'x': 10, 'y': 20 }
+print('x' in d )
+print('a' in d )
+
+# 辞書型は参照渡し
+print('*****x=yとした場合*****')
+x = {'a': 1}
+y = x
+y['a'] = 1000
+print('x→',x )
+print('y→',y)
+
+print('*****x.copyとした場合*****')
+x = {'a': 1}
+y = x.copy()
+y['a'] = 1000
+print('x→',x )
+print('y→',y)
---
+++ Sec4.DataStruc/lesson7(set).py
@@ -0,0 +1,31 @@
+# 集合型
+a = { 1, 1, 2 ,2, 3, 3, 3, 4, 6, 6, 6, 7}
+print(a)
+
+b = { 1, 2, 2, 3, 3, 5}
+print(b)
+print(a-b)
+print(b-a)
+print(a&b)
+print(a|b)
+print(a^b)
+
+# 集合のメソッド
+s = { 1, 2, 3, 4, 5}
+# s[0] エラー
+s.add(6)
+print(s)
+s.remove(6)
+print(s)
+s.clear()
+print(s)
+
+#　集合の使い所
+my_friends = { 'a', 'c', 'd', 'f' }
+A_friends = { 'e' , 'f', 'g' }
+
+print(my_friends & A_friends)
+
+fruits = ['apple','orange','banana','banana','orange']
+kind = set(fruits)
+print(kind)
