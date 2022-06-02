ba4804cc80bf1d0c1a61db9d6f04d39b2f3283ce
macmakobaseballer <mb2.cacs.2367@gmail.com>
2022-06-02 14:52:42 +0900

    [add]while文・完了

--- Sec5.ControlFlow,CodeStructure/lesson2(while).py
+++ Sec5.ControlFlow,CodeStructure/lesson2(while).py
@@ -11,3 +11,29 @@ while True:
     break
   print(count)
   count += 1
+
+# while else文
+count = 0
+while count < 5 :
+  print(count)
+  count += 1
+else:
+  print('done')
+
+# whileの文中でbreakするとelseの中身は処理されず終了
+count = 0
+while count < 5 :
+  if count == 2 :
+    break
+  print(count)
+  count += 1
+else:
+  print('done')
+
+# input関数
+while True:
+  word = input('Enter:')
+  num = int(word)
+  if num == 10:
+    break
+  print('next')
