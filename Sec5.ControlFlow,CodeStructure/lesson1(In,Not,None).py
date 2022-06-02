ea6d0191a9d1830154a9eed0c67d24a73f4052e0
MACBOOK_MAKO <>
2022-06-02 14:34:56 +0900

    [new]In,Not,None・while文

---
+++ Sec5.ControlFlow,CodeStructure/lesson1(In,Not,None).py
@@ -0,0 +1,44 @@
+y = [1, 2, 3]
+x = 1
+
+if x in y:
+  print('in')
+
+if 11 not in y:
+  print('not in')
+
+
+is_ok = True
+
+if not is_ok:
+  print('hello')
+
+is_ok = 0
+
+if is_ok:
+  print('OK!')
+else:
+  print('No!')
+
+is_ok = ''
+
+if is_ok:
+  print('OK!')
+else:
+  print('No!')
+
+is_ok = []
+
+if is_ok:
+  print('OK!')
+else:
+  print('No!')
+
+
+# None オブジェクト
+is_empty = None
+
+if is_empty is None:
+  print('None!!')
+
+
---
+++ Sec5.ControlFlow,CodeStructure/lesson2(while).py
@@ -0,0 +1,13 @@
+# whileループ
+count = 0
+
+while count < 5 :
+  print(count)
+  count += 1
+
+count = 0
+while True:
+  if count >= 5 :
+    break
+  print(count)
+  count += 1
