601782835c1f14d32ef021b3424204da7992aa92
macmakobaseballer <mb2.cacs.2367@gmail.com>
2022-06-03 11:39:02 +0900

    [new]関数定義

---
+++ Sec5.ControlFlow,CodeStructure/lesson5(def).py
@@ -0,0 +1,33 @@
+import math
+
+# 関数定義
+def say_something():
+    print('hi')
+
+say_something()
+f = say_something
+f()
+print(type(f))
+
+# 戻り値
+def calc_tax():
+    price = 100
+    tax_rate = 1.1
+    total_price = math.floor(price * tax_rate)
+    return total_price
+
+result = calc_tax()
+print(result)
+
+# 引数
+def what_is_this(color):
+    if color == 'red':
+        return 'tomato'
+    elif color == 'yellow':
+        return 'banana'
+    else:
+        return "Don't exist"
+
+result = what_is_this('yellow')
+result = what_is_this('green')
+print(result)
