1667e5ea1ba977f3cebacf1a7edb151bd281fa79
macmakobaseballer <mb2.cacs.2367@gmail.com>
2022-06-05 16:49:01 +0900

    [add]引数[WIP]

--- Sec5.ControlFlow,CodeStructure/lesson6(argument).py
+++ Sec5.ControlFlow,CodeStructure/lesson6(argument).py
@@ -15,3 +15,33 @@ def family(father='Ken',mother='Judy'):
 
 family()
 family(father='Cony')
+
+# デフォルト引数の注意点
+# ## デフォルト引数にリストやDicを置くとバグにつながるので控える
+# def test_func(x, l=[]):
+#     l.append(x)
+#     return l
+
+# y = [1,2,3]
+# r = test_func(100, y)
+# print(r)
+
+# r = test_func(100)
+# print(r)
+
+# r = test_func(100)
+# print(r)
+
+## 解決策
+def test_func(x, l=None):
+    if l is None:
+        l = []
+    l.append(x)
+    return l
+
+r = test_func(100)
+print(r)
+
+r = test_func(100)
+print(r)
+
